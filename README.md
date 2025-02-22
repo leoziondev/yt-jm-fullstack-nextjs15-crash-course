6bb216a8cd5adfc2d20dd7cb70dbdfb40427b5ae
leoziondev <leozionsuporte@gmail.com>
2025-02-22 09:08:31 -0300

    feat: create startup card component and add in home page

--- README.md
+++ README.md
@@ -18,4 +18,7 @@ npm run dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+### Reference Youtube Tutorial
+[Next.js 15 Crash Course | Build and Deploy a Production-Ready Full Stack App](https://www.youtube.com/watch?v=Zq5fmkH0T78&t=650s)
+
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query
+
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: { _id: 1, name: 'Adrian' },
+        _id: 1,
+        description: "This is a description",
+        image: "https://images.unsplash.com/photo-1589254065878-42c9da997008?q=80&w=670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        category: "Robots",
+        title: "We Robots"
+    }]
 
   return (
     <div>
@@ -14,6 +26,22 @@ export default async function Home({ searchParams }: {
                 Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
             </p>
             <SearchForm query={query} />
+        </section>
+
+        <section className="section_container">
+            <p className="text-30-semibold">
+                {query ? `Search results for "${query}"` : 'All Startups'}
+            </p>
+
+            <ul className="mt-7 card_grid">
+                {posts.length > 0 ? (
+                    posts.map((post) => (
+                        <StartupCard key={post._id} post={post} />
+                    ))
+                ) : (
+                    <p>Not found results</p>
+                )}
+            </ul>
         </section>
 
     </div>
