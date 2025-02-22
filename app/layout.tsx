06c23a1d60dd1a3e2f8c013133a3e0fdf11ee291
leoziondev <leozionsuporte@gmail.com>
2025-02-22 13:31:33 -0300

    fix: adjust files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+import React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
+import 'easymde/dist/easymde.min.css';
+
 
 export const metadata: Metadata = {
   title: "YC Directory",
